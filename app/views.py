8240b62372b8c98049dfe3975fbe45682ee37ae5
Eric Schles <ericschles@gmail.com>
2016-12-03 12:35:57 -0500

    setting up a simple example of how to do front end dev stuff

--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
 from app import app, db
-from flask import request
+from flask import request, render_template
 from flask_cors import cross_origin
 import json
-
-
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return "server is running"
+
+@app.route("/whatever",methods=["GET","POST"])
+def whatever():
+    whatever = True
+    blarg ="yes, yes he sucks"
+    reasons = [1,2,3,4]
+    reasons.append(5)
+    return render_template("whatever.html",reasons=reasons,blarg=blarg,whatever=whatever)
 
