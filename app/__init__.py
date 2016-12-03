e6859c62bed102c397e0247a882f2813445ae837
Eric Schles <ericschles@gmail.com>
2016-12-03 12:02:09 -0500

    fixing issues with cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
 from .commands import REPL
 import os
 
@@ -11,7 +12,7 @@ app.config["SECRET_KEY"] = "testing"
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://eric_s:1234@localhost/testing"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
-cors = CORS(app, resources={"/send_data":{"origins":"serene-reef-39081.herokuapp.com"})
+cors = CORS(app, resources={"/send_data":{"origins":"serene-reef-39081.herokuapp.com"}})
 
 app.config["CORS_HEADERS"] = 'Content-Type'
 headers = {
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask import request
 from flask_cors import cross_origin
 import json
-import tools
+
 
 
 @app.route("/",methods=["GET","POST"])
